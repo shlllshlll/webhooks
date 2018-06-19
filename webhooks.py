ce6a5ac866707f5a02a4ed9b46df42dde5b2bc01
SHLLL <shlll7347@gmail.com>
2018-06-20 01:19:06 +0800

    :sparkles: Add deploy and config file.

--- webhooks.py
+++ webhooks.py
@@ -3,7 +3,7 @@
 # @Email: shlll7347@gmail.com
 # @Date:   2018-06-17 15:07:16
 # @Last Modified by:   SHLLL
-# @Last Modified time: 2018-06-18 13:58:50
+# @Last Modified time: 2018-06-19 11:45:32
 # @License: MIT LICENSE
 
 import json
@@ -19,17 +19,25 @@ urls = (
 )
 
 app = web.application(urls, globals())
+application = app.wsgifunc()
 render = web.template.render("templates/")
 
 with open('config.json', 'r') as config_j:
     config = json.load(config_j)
 
 
-def git_pull_in_thread(repo_path, stat):
+def git_pull_in_thread(config, stat):
     stat['stat'] = 1
-    repo = Repo(repo_path)
-    origin = repo.remote()
-    origin.pull()
+    repo = Repo(config['path'])
+    repo.heads[config['branch']].checkout()
+    if config['force']:
+        git = repo.git
+        git.fetch('--all')
+        git.reset('--hard', 'origin/master')
+        git.pull()
+    else:
+        origin = repo.remote()
+        origin.pull()
     stat['time'] = time.strftime(
         '%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
     stat['stat'] = 2
@@ -49,10 +57,14 @@ class blog(object):
 
     def POST(self):
         blog.data = web.data().decode()
-        git_thread = threading.Thread(target=git_pull_in_thread, args=(
-            config['blog']['path'], blog.thread_stat,), name="BlogGitThread")
-        git_thread.setDaemon(True)
-        git_thread.start()
+        data_dict = json.loads(blog.data)
+        if data_dict['ref'] == config['blog']['ref']:
+            git_thread = threading.Thread(
+                target=git_pull_in_thread,
+                args=(config['blog'], blog.thread_stat,),
+                name="BlogGitThread")
+            git_thread.setDaemon(True)
+            git_thread.start()
         return 'Done.'
 
 
